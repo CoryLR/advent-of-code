f85f146a2033471100e660598078f37194a50450
CoryLR <cory.rahman@gmail.com>
2025-02-15 08:14:26 -0500

    Progress save

---
+++ 2023/day01/src/main.rs
@@ -0,0 +1,9 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+
+fn main() {
+    let file = File::open("./input.txt");
+    // let reader = io::BufReader::new(file);
+    // file.
+    
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# advent-of-code
+# Advent of Code
+
 Advent of Code 2023+
+
+## Notes
+
+- https://adventofcode.com/
+- Log in via GitHub
